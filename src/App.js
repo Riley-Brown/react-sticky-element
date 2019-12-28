33273ecf744a3a05390de4cffb1b9070767093a4
Riley Brown <riley@riley.gg>
2019-12-28 16:56:59 -0500

    added basic demo functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import useStickyElement from "./useStickyElement";
+import Content from "./Content";
 
 function App() {
+  const myRef = useRef();
+
+  const isSticky = useStickyElement({
+    element: myRef,
+    activeClass: "active-sticky"
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="parent-element">
+        <div
+          ref={myRef}
+          className="sticky-header"
+          style={{ padding: isSticky ? "2rem 0" : "1px 0 0 0" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1>Hey There</h1>
+          <h1>Active sticky: {isSticky.toString()}</h1>
+
+          <span className="animated-line" />
+        </div>
+        <Content />
+      </div>
+    </>
   );
 }
 
---
+++ src/Content.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+export default function Content() {
+  return (
+    <div>
+      <h1>Look at my cool doggie</h1>
+      <img
+        style={{
+          display: "block",
+          maxHeight: "800px",
+          objectFit: "cover"
+        }}
+        src={
+          "https://pbs.twimg.com/media/EMqz_AFWkAAPPqU?format=jpg&name=4096x4096"
+        }
+        alt=""
+      />
+      <h1> Some more content</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat minima
+        sit culpa, expedita a amet magnam explicabo molestias repellendus
+        assumenda quam beatae quasi, voluptas earum magni suscipit rerum
+        corporis impedit, reiciendis adipisci dolore atque? Omnis recusandae
+        dolores magni aliquid suscipit? Labore itaque eveniet beatae doloremque
+        dolorem adipisci aliquam quo quam dignissimos error, esse deserunt
+        accusamus nisi tempora impedit voluptatem sequi illo dolore iusto
+        repellendus repudiandae cupiditate eaque? Quasi, temporibus atque
+        dolorum quos quas est unde laudantium qui, molestias mollitia aut
+        perspiciatis explicabo minus ipsa fugit eaque alias obcaecati illum odit
+        ut officiis? Dolorem recusandae animi unde dolorum nisi itaque possimus.
+      </p>
+    </div>
+  );
+}
